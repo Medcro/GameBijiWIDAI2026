3beb37f380ddb6fa9f610d06d2a73c565dffcf8e
luis <brahmanaluismatthew@gmail.com>
2026-06-17 22:51:16 +0700

    Dash facing bug
    Fix dash now ignore input movement key on early dash

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,24 +10,27 @@ extends CharacterBody2D
 @export var dash_speed : float = 1200.0
 @export var dash_duration : float = 0.2
 @export var dash_cooldown : float = 0.25 # Variabel bebas (boleh diganti angkanya kalo rasanya kelamaan)
-var dash_timer : float = 0.0
-var dash_cooldown_timer : float = 0.0
+var dash_timer : float = 0.0 
+var dash_cooldown_timer : float = 0.0 # init
 var is_dashing : bool = false
 var facing_direction : float = 1.0 # positif -> kanan, negatif -> kiri
 
+# Essence Attributes
+@export var has_agility_essence : bool = false 
+@export var has_flight_essence : bool = false  
 
-# Invicibility Gate (later implement)
-@export var has_agility_essence : bool = false # Check Agility Essence use or not?
 var is_invincible : bool = false 
+var can_double_jump : bool = false 
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction != 0:
+	if direction != 0 and not is_dashing:
 		facing_direction = sign(direction)
 	
-	# dash delay
+	# Dash delay
 	if dash_cooldown_timer > 0:
 		dash_cooldown_timer -= delta
+		
 	if is_dashing:
 		dash_timer -= delta
 		if dash_timer <= 0:
@@ -45,11 +48,18 @@ func _physics_process(delta: float) -> void:
 
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		# can double jump
+		can_double_jump = true
 
-	if Input.is_action_just_pressed("Jump") and is_on_floor() and not is_dashing:
-		velocity.y = jump
-	
-	# Movement
+	if Input.is_action_just_pressed("Jump") and not is_dashing:
+		if is_on_floor():
+			velocity.y = jump
+		elif can_double_jump and has_flight_essence:
+			velocity.y = jump
+			can_double_jump = false 
+
+	# Movement 
 	if is_dashing:
 		velocity.x = facing_direction * dash_speed
 		velocity.y = 0 
