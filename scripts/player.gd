2f76883f819b52eb7e4b289d1fc03a594006653a
luis <brahmanaluismatthew@gmail.com>
2026-06-17 20:14:56 +0700

    Dash mechanic
    Added dash mechanic

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,32 +3,63 @@ extends CharacterBody2D
 # Base Attribute
 @export var hp : int = 5
 @export var speed : float = 500
-@export var jump : float = -300
+@export var jump : float = -350
 @export var gravity : float = 980 
 
 # Dash Attribute
 @export var dash_speed : float = 1200.0
 @export var dash_duration : float = 0.2
+@export var dash_cooldown : float = 0.25 # Variabel bebas (boleh diganti angkanya kalo rasanya kelamaan)
 var dash_timer : float = 0.0
+var dash_cooldown_timer : float = 0.0
 var is_dashing : bool = false
-var facing_direction : float = 1.0
+var facing_direction : float = 1.0 # positif -> kanan, negatif -> kiri
+
+
+# Invicibility Gate (later implement)
+@export var has_agility_essence : bool = false # Check Agility Essence use or not?
+var is_invincible : bool = false 
 
 func _physics_process(delta: float) -> void:
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction != 0:
+		facing_direction = sign(direction)
+	
+	# dash delay
+	if dash_cooldown_timer > 0:
+		dash_cooldown_timer -= delta
+	if is_dashing:
+		dash_timer -= delta
+		if dash_timer <= 0:
+			is_dashing = false
+			is_invincible = false 
+			dash_cooldown_timer = dash_cooldown
+			
+	if Input.is_action_just_pressed("Dash") and not is_dashing and dash_cooldown_timer <= 0:
+		is_dashing = true
+		dash_timer = dash_duration
+		
+		if has_agility_essence:
+			is_invincible = true
+			pass # add visual later
+
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
+
+	if Input.is_action_just_pressed("Jump") and is_on_floor() and not is_dashing:
 		velocity.y = jump
 	
-	var direction = Input.get_axis("ui_left", "ui_right")
-	
-	if is_on_floor():
-		if direction:
-			velocity.x = direction * speed
-		else:
-			velocity.x = move_toward(velocity.x, 0, speed)
-			
+	# Movement
+	if is_dashing:
+		velocity.x = facing_direction * dash_speed
+		velocity.y = 0 
 	else:
-		velocity.x = lerp(velocity.x, direction*speed, delta * 2.0)
-		
+		if is_on_floor():
+			if direction:
+				velocity.x = direction * speed
+			else:
+				velocity.x = move_toward(velocity.x, 0, speed)
+		else:
+			velocity.x = lerp(velocity.x, direction * speed, delta * 2.0)
+			
 	move_and_slide()
