7d9826d55589b83bc11768bdc4cb8664a93d1e4a
Herrscher Erlkonig-Kaiser INAN <anonmakantahubulat@gmail.com>
2026-06-20 22:31:09 +0700

    added level system base (belum bisa ngapa-ngapain for now)

---
+++ scripts/room_data.gd
@@ -0,0 +1,17 @@
+extends Resource
+class_name RoomData
+
+enum Type { SPAWN, PLATFORM, MINION, TREASURE, PASSIVE, BOSS }
+
+var grid_position: Vector2i
+var type: Type
+var variation_id: int
+var scene_path: String
+
+## buat minimap
+#var is_discovered: bool = false
+#var is_cleared: bool = false
+
+func _init(pos: Vector2i, t: Type) -> void:
+	grid_position = pos
+	type = t
