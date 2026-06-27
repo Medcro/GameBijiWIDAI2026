9c022b136bdae8b7f204b8234611346362282c96
Herrscher Erlkonig-Kaiser INAN <anonmakantahubulat@gmail.com>
2026-06-27 19:21:57 +0700

    level manager integration prototype

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -18,7 +18,18 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed() -> void:
 	$Click.play()
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	LevelManager.current_level_num = 1
+	LevelManager.generate_new_level()
+
+	# 2. Get the room data for the spawn room
+	var spawn_coords = LevelManager.current_room_coords
+	var spawn_room = LevelManager.current_map[spawn_coords]
+
+	# 3. Transition to the scene file chosen for the spawn room!
+	if spawn_room and spawn_room.scene_path != "":
+		get_tree().change_scene_to_file(spawn_room.scene_path)
+	else:
+		push_error("Failed to find a valid scene path for the Spawn room!")
 
 func _on_settings_pressed() -> void:
 	$Click.play()
--- scripts/room_data.gd
+++ scripts/room_data.gd
@@ -9,8 +9,8 @@ var variation_id: int
 var scene_path: String
 
 ## buat minimap
-#var is_discovered: bool = false
-#var is_cleared: bool = false
+var is_discovered: bool = false
+var is_cleared: bool = false
 
 func _init(pos: Vector2i, t: Type) -> void:
 	grid_position = pos
